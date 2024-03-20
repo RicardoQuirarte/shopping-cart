05df93e139c7ffe4a01f9354cfb57be607126b8c
Ricardo Quirarte <reynaldo.quirarte@gmail.com>
2024-03-19 23:10:09 -0600

    plus, minus and add to car buttons working fine at the moment

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,20 +1,13 @@
 import { useState, useEffect } from "react";
 import "/src/App.css";
 import NavBar from "../components/NavBar";
-import { Link } from "react-router-dom";
-import { createContext } from "react";
-
-export const cartContext = createContext();
 
 function Items() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [quantity, setQuantity] = useState([]);
   const [car, setCar] = useState(0);
   const [carItems, setCarItems] = useState();
-
-  console.log(carItems);
 
   useEffect(() => {
     const getProducts = async () => {
@@ -31,54 +24,35 @@ function Items() {
         }
         return cart;
       };
-
       setCarItems(defaultCart());
 
-      setQuantity(
-        products.map((elem) => ({
-          id: elem.id,
-          quantity: 0,
-        }))
-      );
       setLoading(false);
     };
     getProducts();
   }, []);
 
   function minus(id) {
-    if (Object.values(quantity).indexOf(id) < 0) return;
-
-    const newQuantity = Object.values(quantity).indexOf(id) - 1;
-
-    // const newObject = { ...quantity, {id: id, quantity - 1}}
-
-    const updatedQuantity = { id: quantity };
-    setQuantity(newQuantity);
+    if (carItems[id] <= 0) return;
+    setCarItems((prev) => ({ ...prev, [id]: prev[id] - 1 }));
   }
 
   function plus(id) {
-    // const newQuantity = quantity + 1;
-    // setQuantity(newQuantity);
-
     setCarItems((prev) => ({ ...prev, [id]: prev[id] + 1 }));
     console.log(carItems);
   }
 
-  function handleInput(e, index) {
-    const newValue = [...quantity];
-    newValue[index] = Number(e.target.value);
-    setQuantity(newValue);
+  function handleInput(id, e) {
+    // const newValue = [...quantity];
+    // newValue[index] = Number(e.target.value);
+    // setQuantity(newValue);
 
     // setQuantity(Number(e.target.value));
+
+    setCarItems((prev) => ({ ...prev, [id]: (prev[id] = e.target.value) }));
   }
 
-  function addToCar(item) {
-    // const newCar = car + quantity;
-    // setCar(newCar);
-
-    const shopItems = [];
-    shopItems.push(item);
-    setCarItems(shopItems);
+  function addToCar() {
+    setCar(Object.values(carItems).reduce((a, b) => a + b, 0));
   }
 
   return (
@@ -88,16 +62,13 @@ function Items() {
         <p>Loading...</p>
       ) : (
         <div className="items">
-          {data.map((item, index) => (
+          {data.map((item) => (
             <div className="card" key={item.id}>
               <p className="title">{item.title}</p>
               <img src={item.image} alt={item.title} />
               <p>Price: {`$${item.price}`}</p>
               <p className="description">{item.description}</p>
-              <Link to={{ pathname: "/shopppingCart", state: item }}>
-                Add to car2
-              </Link>
-              <button onClick={() => addToCar(item)}>Add to car</button>
+              <button onClick={addToCar}>Add to car</button>
               <div className="add-to-car">
                 <img
                   className="minus-plus"
@@ -108,8 +79,8 @@ function Items() {
                 <input
                   type="text"
                   id="amount"
-                  value={5}
-                  onChange={() => handleInput(index)}
+                  value={carItems[item.id]}
+                  onChange={() => handleInput(item.id, e)}
                 />
                 <img
                   className="minus-plus"
